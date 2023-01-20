f7bc3eeb8a5a20999159dffc477de51bc7fdb1ec
Raviteja-ust <121094220+Raviteja-ust@users.noreply.github.com>
2023-01-20 14:48:20 +0530

    Add files via upload

---
+++ SampleApp/src/main/java/com/ust/app/pojo/Student.java
@@ -0,0 +1,15 @@
+package com.ust.app.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+    private int id;
+    private String name;
+    private String standard;
+    private int marks;
+}
